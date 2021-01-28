2e753aa05d34741302776cb1a50c38b9f3b20a96
Peter Schaldenbrand <pls21@pitt.edu>
2021-01-27 20:21:07 -0500

    Object detection test removed

--- train_sketchy_classifier.py
+++
@@ -1,5 +0,0 @@
-from sketchy.classifier import SketchyClassifier
-print(SketchyClassifier.class_names)
-print(SketchyClassifier.sketchy_img_dir)
-classifier = SketchyClassifier(model='robustness')
-classifier.train(200,batch_size=32)
